989364805f937e0d25dcea1c877644e91eb45b12
Harsh Biyani <38360887+Mitro007@users.noreply.github.com>
2018-09-03 21:51:14 +0200

    Add files via upload
    QuickSort algorithm

---
+++ QuickSort.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class QuickSort {
+	
+	public static int partition(int[] a,int start,int end) {
+		int pivot=a[end];
+		int pIndex=start;
+		for(int i=start;i<end;i++) {
+			if(a[i]<pivot) {
+				// swap a[i] and a[pIndex]
+				a[i]=a[i]^a[pIndex]^(a[pIndex]=a[i]);
+				pIndex++;
+			}
+		}
+		//swap a[pIndex] and pivot
+		a[end]=a[pIndex];
+		a[pIndex]=pivot;
+		return pIndex;
+	}
+	
+	public static void quickSort(int[] a,int start,int end) {
+		if(start < end) {
+			int pIndex = partition(a, start, end);
+			quickSort(a, start, pIndex-1);
+			quickSort(a, pIndex+1, end);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Random gen = new Random();
+		int[] a = new int[10];
+		for(int i=0;i<a.length;i++) {
+			int input=gen.nextInt(100);
+			a[i]=input;
+		}
+		System.out.println("Before Sorting :");
+		System.out.println(Arrays.toString(a));
+		System.out.println("After Sorting");
+		quickSort(a,0,a.length-1);
+		System.out.println(Arrays.toString(a));
+	}
+}
