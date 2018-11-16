acf672b304afa407227a3f8f180d6aa5cae4bc1c
Harsh Biyani <38360887+Mitro007@users.noreply.github.com>
2018-11-16 07:32:01 +0100

    Add files via upload

--- CountingSort.java
+++ CountingSort.java
@@ -1,28 +1,73 @@
+package sorting;
+
 import java.util.Arrays;
+import java.util.Random;
 
 public class CountingSort {
 
-	public static void countingSort(int[] a) {
-		int[] count=new int[31];
-		int[] result=new int[a.length];
+	public static int maxElement(int[] a) {
+		int max = 0;
+		for(int i=1;i<a.length;i++) {
+			if(a[i]>a[max])
+				max=i;
+		}
+		return a[max];
+	}
+	
+	public static int minElement(int[] a) {
+		int min = 0;
+		for(int i=1;i<a.length;i++) {
+			if(a[i] < a[min])
+				min=i;
+		}
+		return a[min];
+	}
+	
+	// written by me
+	/*public static int[] countingSort(int[] a) {
+		int count[] = new int[maxElement(a)+1];
+		int[] output = new int[a.length];
 		for(int x:a)
 			count[x]++;
-		System.out.println(Arrays.toString(count));
-		for (int i = 1; i < count.length; i++) {
-			count[i]=count[i]+count[i-1];
+		//System.out.println(Arrays.toString(count));
+		int minIndex = minElement(a);
+		int maxIndex = maxElement(a);
+		int k=0;
+		for(int i=minIndex;i<=maxIndex;i++) {
+			while(count[i] > 0) {
+				output[k++]=i;
+				count[i]--;
+			}
 		}
-		System.out.println(Arrays.toString(count));
+		return output;
+	}*/
+	
+	// actual implementation
+	public static int[] countingSort(int[] a) {
+		int[] count = new int[maxElement(a)+1];
+		int[] output = new int[a.length];
+		for(int x:a)
+			count[x]++;
+		for(int i=1;i<count.length;i++)
+			count[i]=count[i]+count[i-1];
 		for(int x:a) {
-			result[count[x]-1]=x;
+			int index = count[x];
+			output[--index]=x;
 			count[x]--;
 		}
-		System.out.println(Arrays.toString(result));
+		return output;
 	}
 	
 	public static void main(String[] args) {
-		int[] a= {2,27,22,8,28,1,30,8,29,28,15,16,25,28,23,16,24,23,17,11,4,2,2,22,23,8,29,16
-				  ,29,26};
-		System.out.println(Arrays.toString(a));
-		countingSort(a);
+		Random gen = new Random();
+		int[] input = new int[10];
+		for (int i = 0; i < input.length; i++) {
+			input[i]=gen.nextInt(100);
+		}
+		System.out.println("Array before sorting : "+ Arrays.toString(input));
+		//System.out.println(maxElement(input));
+		//System.out.println(minElement(input));
+		System.out.println("Array after sorting : "+ Arrays.toString(countingSort(input)));
+		
 	}
 }
